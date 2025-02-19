ddce3e4eaaadcf368a24e4c48b506edbf5f0224c
344-novice <344literature@gmail.com>
2025-02-19 17:57:19 +0000

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL = "https://344-novice.github.io/tech-blog.github.io/";
